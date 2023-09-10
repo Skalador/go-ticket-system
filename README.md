f8fa3c89ac9a2f240c1ed509463efd4dfe8a7426
Skalador <k.niederwanger@gmail.com>
2023-09-10 19:41:12 +0200

    Adding database connectivity

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # GO-Ticket-System
 
 In this Repo a GO web application is built to have the basic functionality of a ticketing system.
+
+Run the code:
+```
+go run main.go
+```
+
+
+Installing the mongoDB driver:
+```
+go get go.mongodb.org/mongo-driver/mongo
+```
+
+MongoDB is hosted for free on Atlas. The Data can be visualized with `MongoDBCompass`.
--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"text/template"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const PORT = ":8000"
+const CONNECTIONSTRING = "mongodb+srv://kniederwanger:bat1OSclL7elzT0h@test-cluster.bwnvnol.mongodb.net/?retryWrites=true&w=majority"
+//var client *mongo.Client
 
 type Ticket struct {
 	Subject string
@@ -96,7 +103,34 @@ func submitHandler(w http.ResponseWriter, req *http.Request) {
     http.Redirect(w, req, "/", http.StatusSeeOther)
 }
 
+
 func main() {
+
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(CONNECTIONSTRING).SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	
+	if err != nil {
+	  panic(err)
+	}
+	defer func() {
+	  if err = client.Disconnect(context.TODO()); err != nil {
+		panic(err)
+	  }
+	}()
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+	  panic(err)
+	}
+	log.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	//Todo: Create Database with GO
+	//Create Collection with GO
+	//Upload data.json with GO
+	//Read database with GO
+	
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static")))) //Fileserver to load css
 	http.HandleFunc("/",rootHandler)
